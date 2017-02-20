8f1266e1d8059a31c233ba737a9ba61ebc90a3b1
JacaMan2097 <holandakassio@hotmail.com>
2017-02-20 09:33:24 -0300

    Atividades

---
+++ Atividades_POO/QuestesJava8/src/Cap05/ArrayNumeros.java
@@ -0,0 +1,15 @@
+package Cap05;
+
+import java.text.DecimalFormat;
+
+public class ArrayNumeros {
+	public static void main(String[] args) {
+		int[] inteiro = new int[10];
+		DecimalFormat df = new DecimalFormat();
+		df.applyPattern("0000");
+		for (int i = 0; i < inteiro.length; i++) {
+			inteiro[i] = (int) (Math.random() * 1000);
+			System.out.println(df.format(inteiro[i]));
+		}
+	}
+}
