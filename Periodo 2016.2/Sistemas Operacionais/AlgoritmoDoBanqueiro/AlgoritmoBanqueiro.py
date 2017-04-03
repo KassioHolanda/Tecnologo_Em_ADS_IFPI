c91c1bbd206bfcee32d8c63c04281eb450fc27dc
JacaMan2097 <holandakassio@hotmail.com>
2017-04-03 11:17:14 -0300

    Atualizacao

--- Periodo 2016.2/Sistemas Operacionais/AlgoritmoDoBanqueiro/AlgoritmoBanqueiro.py
+++ Periodo 2016.2/Sistemas Operacionais/AlgoritmoDoBanqueiro/AlgoritmoBanqueiro.py
@@ -1,5 +1,5 @@
 def main():
-
+    resultado = 0
     existentes = [5,2,3,1]
     disponiveis = [3,1,1,0]
 
@@ -7,24 +7,26 @@ def main():
         [0,0,1,0],
         [1,0,0,0],
         [1,1,1,1],
-        # [1,0,1,0]
+        [1,0,1,0]
     ]
 
     # 4 2 2 1
     # 4 2 3 1
     # 5 2 3 1
 
-    resultado = 0
-
     requisitados = [
         [1,0,0,0],
         [1,1,2,0],
         [0,0,0,0],
-        # [1,1,1,0]
+        [6,1,1,0]
     ]
 
-    for i in range(len(disponiveis)-1):
-        for j in range(len(disponiveis)-1):
+    consultarDeadlock(existentes, disponiveis, alocados, resultado, requisitados)
+
+def consultarDeadlock(existentes, disponiveis, alocados, resultado, requisitados):
+
+    for i in range(len(disponiveis)):
+        for j in range(len(disponiveis)):
             if requisitados[i][0] <= disponiveis[0] and requisitados[i][1] <= disponiveis[1] \
                     and requisitados[i][2] <= disponiveis[2] and requisitados[i][3] <= disponiveis[3]:
                 disponiveis[0] += alocados[i][0]
@@ -34,16 +36,16 @@ def main():
                 resultado += 1
                 break
 
-    if resultado == 3 and disponiveis[0] == existentes[0] and disponiveis[1] == existentes[1] and disponiveis[2] == existentes[2]: # disponiveis[3] == existentes[3]
+    if resultado == 4 and disponiveis[0] == existentes[0] and disponiveis[1] == existentes[1] and disponiveis[2] == existentes[2] and disponiveis[3] == existentes[3]:
 
         print disponiveis[0] == existentes[0]
         print disponiveis[1] == existentes[1]
         print disponiveis[2] == disponiveis[2]
 
         print "\n"
-        print "Nao houve DeadLock"
+        print "Sem DeadLock"
     else:
-        print "Houve DeadLock"
+        print "DeadLock"
 
 if __name__ == "__main__":
     main()
