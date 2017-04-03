e824abbce2d9db11d0903b613880c0d002f913a0
JacaMan2097 <holandakassio@hotmail.com>
2017-04-03 11:37:20 -0300

    Atualizacao

--- Periodo 2016.2/Sistemas Operacionais/AlgoritmoDoBanqueiro/AlgoritmoBanqueiro.py
+++ Periodo 2016.2/Sistemas Operacionais/AlgoritmoDoBanqueiro/AlgoritmoBanqueiro.py
@@ -1,4 +1,5 @@
 def main():
+    print "\n\nmain"
     resultado = 0
     existentes = [5,2,3,1]
     disponiveis = [3,1,1,0]
@@ -7,7 +8,7 @@ def main():
         [0,0,1,0],
         [1,0,0,0],
         [1,1,1,1],
-        [1,0,1,0]
+        [0,0,0,0]
     ]
 
     # 4 2 2 1
@@ -18,12 +19,13 @@ def main():
         [1,0,0,0],
         [1,1,2,0],
         [0,0,0,0],
-        [6,1,1,0]
+        [1,1,1,0]
     ]
 
-    consultarDeadlock(existentes, disponiveis, alocados, resultado, requisitados)
+    consultar_matrizes(existentes, disponiveis, alocados, resultado, requisitados)
 
-def consultarDeadlock(existentes, disponiveis, alocados, resultado, requisitados):
+def consultar_matrizes(existentes, disponiveis, alocados, resultado, requisitados):
+    print "\n\nConsultar matrizes"
 
     for i in range(len(disponiveis)):
         for j in range(len(disponiveis)):
@@ -35,16 +37,21 @@ def consultarDeadlock(existentes, disponiveis, alocados, resultado, requisitados
                 disponiveis[3] += alocados[i][3]
                 resultado += 1
                 break
+    consultar_deadlock(existentes, disponiveis, resultado)
+
+def consultar_deadlock(existentes, disponiveis, resultado):
+
+    print "\n\nConsultar deadlock"
+    print disponiveis[0] == existentes[0]
+    print disponiveis[1] == existentes[1]
+    print disponiveis[2] == existentes[2]
+    print disponiveis[3] == existentes[3]
 
     if resultado == 4 and disponiveis[0] == existentes[0] and disponiveis[1] == existentes[1] and disponiveis[2] == existentes[2] and disponiveis[3] == existentes[3]:
-
-        print disponiveis[0] == existentes[0]
-        print disponiveis[1] == existentes[1]
-        print disponiveis[2] == disponiveis[2]
-
         print "\n"
         print "Sem DeadLock"
     else:
+        print "\n"
         print "DeadLock"
 
 if __name__ == "__main__":
