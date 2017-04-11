c7f9573c0dba20596150c11d24d841e0ef700381
JacaMan2097 <holandakassio@hotmail.com>
2017-04-11 09:04:56 -0300

    Atualizacao

--- Periodo 2016.2/Estrutura De Dados/Arvores.cpp
+++ Periodo 2016.2/Estrutura De Dados/Arvores.cpp
@@ -120,10 +120,10 @@ class Arvore{
 		
 		void consultar_Completa(No *arv){
 			if(altura_Minima(arv) == altura(arv) + 1){
-				cout<<"\nCompleta\n";
+				cout<<"\nArvore Completa\n";
 
 			} else {
-				cout<<"\nIncompleta\n";
+				cout<<"\nArvore Incompleta\n";
 			}
 		}
 				
@@ -131,6 +131,9 @@ class Arvore{
 
 
 int main(){
+	
+// 	1 - adicionar a esquerda
+//	2 - adicionar a direitra 
 	
 	Arvore *arv = new Arvore();	
 	arv->cria_vazia();
@@ -142,7 +145,10 @@ int main(){
 	No *no_c = arv->cria_No('c', NULL, NULL);
 	No *no_d = arv->cria_No('d', NULL, NULL);
 	No *no_e = arv->cria_No('e', NULL, NULL);
-	No *no_f = arv->cria_No('f', NULL, NULL);	
+	No *no_f = arv->cria_No('f', NULL, NULL);
+	No *no_g = arv->cria_No('g', NULL, NULL);
+	No *no_h = arv->cria_No('h', NULL, NULL);
+		
 	
 	cout<<"Imprimindo a arvore\n";
 	arv->imprime(arv->raiz);
@@ -157,12 +163,19 @@ int main(){
 	arv->insere(no_a, no_c, 1, no_a->info);
 	arv->insere(no_a, no_d, 2, no_a->info);
 	
-
+	//adicionando no no B
+	arv->insere(no_b, no_e, 1, no_b->info);
+	arv->insere(no_b, no_f, 2, no_b->info);
+	
+	//adicionando no no F
+	arv->insere(no_f, no_g, 2, no_f->info);
+	
+	//adicionando no no G
+	arv->insere(no_g, no_h, 2, no_g->info);
 	
 	arv->imprime(raiz);
 	
 	//tamanho da arvore
-	cout<<"\nTamanho da arvore: ";
 	arv->consultar_Completa(arv->raiz);
 	cout<<"\n";
 	
