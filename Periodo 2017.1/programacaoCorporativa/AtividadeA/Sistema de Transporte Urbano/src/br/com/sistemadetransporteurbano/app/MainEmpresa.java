47db70e8fdeacb2aaf1ed3376de6e6c1380fd6e7
JacaMan2097 <holandakassio@hotmail.com>
2017-05-26 10:07:11 -0300

    Atualizacao

--- Periodo 2017.1/programacaoCorporativa/AtividadeA/Sistema de Transporte Urbano/src/br/com/sistemadetransporteurbano/app/MainEmpresa.java
+++ Periodo 2017.1/programacaoCorporativa/AtividadeA/Sistema de Transporte Urbano/src/br/com/sistemadetransporteurbano/app/MainEmpresa.java
@@ -9,21 +9,19 @@ import br.com.sistemadetransporteurbano.domain.Onibus;
 import br.com.sistemadetransporteurbano.domain.Paradas;
 
 public class MainEmpresa {
-	
+
 	public static void main(String[] args) {
 		Onibus onibus = new Onibus(123312);
-		Paradas paradas = new Paradas(13, "SÃ£o Paulo");
+		Paradas paradas = new Paradas(13, "São Paulo");
 		EscalaLinha escalaLinha = new EscalaLinha(onibus, "8:00", paradas);
 		Linha linha = new Linha(escalaLinha, 405);
-		
 
 		EscalaFuncionario escalaFuncionario = new EscalaFuncionario("12:00", linha);
 		Funcionario funcionario = new Funcionario("Kassio", "123", escalaFuncionario);
-		
+
 		EmpresaOnibus empresa = new EmpresaOnibus(funcionario, linha);
-		
-		
-		System.out.println("Bairro Linha de Onibus: " + empresa.getLinha().getEscalaLinha().getParadas().getBairroParada());	
+
+		System.out.println("Bairro Linha de Onibus: " + empresa.getLinha().getEscalaLinha().getParadas().getBairroParada());
 	}
-	
+
 }
