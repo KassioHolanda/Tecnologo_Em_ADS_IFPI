bb2685b92c1f0b5c60efeef11cc06617de04d6ba
KassioHolanda <holandakassio@hotmail.com>
2017-07-19 13:49:16 -0300

    Numeros romanos

--- Periodo 2017.1/programacaoCorporativa/AtividadeF/Projeto Numeros Romanos/src/TesteNumerosRomanos/NumerosRomanosTest.java
+++ Periodo 2017.1/programacaoCorporativa/AtividadeF/Projeto Numeros Romanos/src/TesteNumerosRomanos/NumerosRomanosTest.java
@@ -1,6 +1,7 @@
 package TesteNumerosRomanos;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,15 +11,28 @@ import model.NumerosRomanos;
 public class NumerosRomanosTest {
 
 	NumerosRomanos numerosRomanos;
-	
+
 	@Before
-	public void init(){
+	public void init() {
 		numerosRomanos = new NumerosRomanos();
 	}
-	
-	@Test(expected=NullPointerException.class)
+
+	@Test
 	public void consultarLetrasDentroDoSistemaNumeroRomano() {
-		assertEquals(1, numerosRomanos.pegarNumero("K"), 0);
+		assertEquals(1, numerosRomanos.pegarNumero("I"), 0);
+		assertEquals(5, numerosRomanos.pegarNumero("V"), 0);
+		assertEquals(10, numerosRomanos.pegarNumero("X"), 0);
+		assertEquals(50, numerosRomanos.pegarNumero("L"), 0);
+		assertEquals(100, numerosRomanos.pegarNumero("C"), 0);
+		assertEquals(500, numerosRomanos.pegarNumero("D"), 0);
+		assertEquals(1000, numerosRomanos.pegarNumero("M"), 0);    
+	}
+
+	@Test
+	public void letrasNaoPodemRepetirMaisDeTresVezes() {
+		String romano;
+		
+		
 	}
 
 }
--- Periodo 2017.1/programacaoCorporativa/AtividadeF/Projeto Numeros Romanos/src/model/Main.java
+++ Periodo 2017.1/programacaoCorporativa/AtividadeF/Projeto Numeros Romanos/src/model/Main.java
@@ -1,9 +1,25 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 	public static void main(String[] args) {
 		NumerosRomanos numeros = new NumerosRomanos();
+		Map quantidadeLetras = new HashMap();
 		
-		System.out.println(numeros.pegarNumero("I"));
+		
+		String numero = "XVV";
+		System.out.println(numero.split("")[1]);
+		for(int i = 0; i < numero.length(); i++){
+			///System.out.println(numero.substring(i));
+			if(quantidadeLetras.containsKey(numero.split("")[i])){
+				quantidadeLetras.replace(numero.split("")[i] ), quantidadeLetras.get((numero.split("")[i].hashCode() + 1));
+			} else {
+				quantidadeLetras.put(numero.split("")[i], 1);
+			}
+			
+		}
+		//System.out.println(quantidadeLetras.get("V"));
 	}
 }
